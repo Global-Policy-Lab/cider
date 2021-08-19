b59b1bfb308e6bfd19943bed4ab8b99fc93f0fbf
LucioMelito <lucio.melito@gmail.com>
2021-08-19 18:31:15 +0100

    typehints and docstrings for datastore

--- datastore.py
+++ datastore.py
@@ -3,8 +3,10 @@ from box import Box
 from collections import defaultdict
 from helpers.io_utils import *
 from helpers.opt_utils import *
+from pandas import DataFrame as PandasDataFrame
 import pyspark.sql.functions as F
 from pyspark.sql.functions import col, count, lit
+from typing import Dict, Union
 import yaml
 
 
@@ -44,7 +46,7 @@ class InitializerInterface(ABC):
 
 class DataStore(InitializerInterface):
     def __init__(self, cfg_dir: str):
-        # Read config file
+        # Read config file and store paths
         with open(cfg_dir, "r") as ymlfile:
             cfg = Box(yaml.load(ymlfile, Loader=yaml.FullLoader))
         self.cfg = cfg
@@ -66,6 +68,7 @@ class DataStore(InitializerInterface):
         # Possible datasets
         self.datasets = ['cdr', 'recharges', 'mobiledata', 'mobilemoney']
         self.cdr = None
+        self.cdr_bandicoot = None
         self.recharges = None
         self.mobiledata = None
         self.mobilemoney = None
@@ -74,55 +77,90 @@ class DataStore(InitializerInterface):
         self.ground_truth = None
         self.poverty_scores = None
 
-    def load_cdr(self, dataframe=None):
+    def load_cdr(self, dataframe: Union[SparkDataFrame, PandasDataFrame] = None) -> None:
+        """
+        Load cdr data: use file path specified in config as default, or spark/pandas df
+        Args:
+            dataframe: spark/pandas df to assign if available
+        """
         fpath = self.data + self.file_names.cdr if self.file_names.cdr is not None else None
         if fpath or dataframe is not None:
             print('Loading CDR...')
             cdr = load_cdr(self.cfg, fpath, df=dataframe)
             self.cdr = cdr
-            self.cdr_bandicoot = None
 
-    def load_antennas(self, dataframe=None):
+    def load_antennas(self, dataframe: Union[SparkDataFrame, PandasDataFrame] = None) -> None:
         fpath = self.data + self.file_names.antennas if self.file_names.antennas is not None else None
         if fpath or dataframe is not None:
             print('Loading antennas...')
             self.antennas = load_antennas(self.cfg, fpath, df=dataframe)
 
-    def load_recharges(self, dataframe=None):
+    def load_recharges(self, dataframe: Union[SparkDataFrame, PandasDataFrame] = None) -> None:
+        """
+        Load recharges data: use file path specified in config as default, or spark/pandas df
+        Args:
+            dataframe: spark/pandas df to assign if available
+        """
         fpath = self.data + self.file_names.recharges if self.file_names.recharges is not None else None
         if fpath or dataframe is not None:
             print('Loading recharges...')
             self.recharges = load_recharges(self.cfg, fpath, df=dataframe)
 
-    def load_mobiledata(self, dataframe=None):
+    def load_mobiledata(self, dataframe: Union[SparkDataFrame, PandasDataFrame] = None) -> None:
+        """
+        Load mobile data: use file path specified in config as default, or spark/pandas df
+        Args:
+            dataframe: spark/pandas df to assign if available
+        """
         fpath = self.data + self.file_names.mobiledata if self.file_names.mobiledata is not None else None
         if fpath or dataframe is not None:
             print('Loading mobile data...')
             self.mobiledata = load_mobiledata(self.cfg, fpath, df=dataframe)
 
-    def load_mobilemoney(self, dataframe=None):
+    def load_mobilemoney(self, dataframe: Union[SparkDataFrame, PandasDataFrame] = None) -> None:
+        """
+        Load mobile money data: use file path specified in config as default, or spark/pandas df
+        Args:
+            dataframe: spark/pandas df to assign if available
+        """
         fpath = self.data + self.file_names.mobilemoney if self.file_names.mobilemoney is not None else None
         if fpath or dataframe is not None:
             print('Loading mobile data...')
             self.mobilemoney = load_mobilemoney(self.cfg, fpath, df=dataframe)
 
-    def load_shapefiles(self):
+    def load_shapefiles(self) -> None:
+        """
+        Iterate through shapefiles specified in config and load them in self.shapefiles dictionary
+        """
         # Load shapefiles
         shapefiles = self.file_names.shapefiles
         for shapefile_fname in shapefiles.keys():
             self.shapefiles[shapefile_fname] = load_shapefile(self.data + shapefiles[shapefile_fname])
 
-    def load_home_ground_truth(self):
+    def load_home_ground_truth(self) -> None:
+        """
+        Load ground truth data for home locations
+        """
         if self.file_names.home_ground_truth is not None:
             self.ground_truth = pd.read_csv(self.data + self.file_names.home_ground_truth)
         else:
             print('No ground truth data for home locations has been specified.')
 
-    def load_poverty_scores(self):
+    def load_poverty_scores(self) -> None:
+        """
+        Load poverty scores (e.g. those produced by the ML module)
+        """
         if self.file_names.poverty_scores is not None:
             self.poverty_scores = pd.read_csv(self.data + self.file_names.poverty_scores)
 
-    def load_data(self, module, dataframes=None):
+    def load_data(self, module: str, dataframes:  Dict[Union[SparkDataFrame, PandasDataFrame]] = None) -> None:
+        """
+        Load and process all datasets required by a module
+        Args:
+            module: module to load data for
+            dataframes: dictionary of dataframes to use; if none is passed reads from file paths in config
+        """
+        # Create defaultdict to avoid keyerrors
         dataframes = dataframes if dataframes else defaultdict(lambda: None)
         cdr_df = dataframes['cdr']
         recharges_df = dataframes['recharges']
@@ -184,21 +222,31 @@ class DataStore(InitializerInterface):
 
             elif self.geo != 'antenna_id':
                 raise ValueError('Invalid geography, must be antenna_id, tower_id, or shapefile name')
+        else:
+            raise ValueError(f"The module name provided - '{module}' - is incorrect or not supported.")
 
-    def filter_dates(self, start_date, end_date):
+    def filter_dates(self, start_date: str, end_date: str) -> None:
+        """
+        Filter data outside [start_date, end_date] (inclusive) in all available datasets
+        Args:
+            start_date: e.g. '2020-01-01'
+            end_date: e.g. '2020-01-10'
+        """
         for dataset_name in self.datasets:
             dataset = getattr(self, '_' + dataset_name, None)
             if dataset is not None:
                 setattr(self, dataset_name, filter_dates_dataframe(dataset, start_date, end_date))
 
-    def deduplicate(self):
+    def deduplicate(self) -> None:
+        """
+        Remove duplicate rows from alla available datasets
+        """
         for dataset_name in self.datasets:
             dataset = getattr(self, '_' + dataset_name, None)
             if dataset is not None:
                 setattr(self, dataset_name, dataset.distinct())
 
     def remove_spammers(self, spammer_threshold=100):
-
         # Raise exception if no CDR, since spammers are calculated only on the basis of call and text
         if self.cdr is None:
             raise ValueError('CDR must be loaded to identify and remove spammers.')
@@ -230,7 +278,6 @@ class DataStore(InitializerInterface):
         return self.spammers
 
     def filter_outlier_days(self, num_sds=2):
-
         # Raise exception if no CDR, since spammers are calculated only on the basis of call and text
         if self.cdr is None:
             raise ValueError('CDR must be loaded to identify and remove outlier days.')
@@ -269,7 +316,6 @@ class DataStore(InitializerInterface):
 class OptDataStore(DataStore):
     def __init__(self, cfg_dir: str):
         super(OptDataStore, self).__init__(cfg_dir)
-
         self._user_consent = None
 
     @property
@@ -277,19 +323,33 @@ class OptDataStore(DataStore):
         return self._user_consent
 
     @user_consent.setter
-    def user_consent(self, val):
+    def user_consent(self, val: SparkDataFrame) -> None:
+        """
+        Whenever the user consent table is updated, also update all datasets in the datastore to include only users that
+        have given their consent
+        Args:
+            val: new user consent table as spark df
+        """
         self._user_consent = val
+        # Get name of user id column
         user_col_name = val.columns[0] if val is not None else None
+        # Iterate through all private datasets, update the corresponding visible datasets
         for dataset_name in self.datasets:
             dataset = getattr(self, '_' + dataset_name, None)
             if dataset is not None and val is not None:
                 setattr(self, dataset_name, dataset.join(val.where(col('include') == True).select(user_col_name),
                                                          on=user_col_name, how='inner'))
 
-    def initialize_user_consent_table(self):
+    def initialize_user_consent_table(self) -> None:
+        """
+        Create table of all user ids present in the datasets, and whether they should be included in the analysis or not
+        This is defined by the opt_in_default parameter specified in the config file
+        """
+        # Create internal use datasets which do not get updated based on the consent table
         for dataset_name in self.datasets:
             if getattr(self, dataset_name, None) is not None:
                 setattr(self, '_' + dataset_name, getattr(self, dataset_name))
+        # Get all available datasets and create consent table
         data = []
         for dataset_name in self.datasets:
             if getattr(self, '_' + dataset_name, None) is not None:
@@ -298,13 +358,23 @@ class OptDataStore(DataStore):
         self.user_consent = generate_user_consent_list(data, user_id_col=user_col_name,
                                                        opt_in=self.cfg.params.opt_in_default)
 
-    def opt_in(self, user_ids):
+    def opt_in(self, user_ids: List[str]) -> None:
+        """
+        Update the user consent table based on list of user ids that have opted in
+        Args:
+            user_ids: list of user ids to flag as opted in, i.e. include = True
+        """
         user_col_name = self.user_consent.columns[0]
         self.user_consent = (self.user_consent
                              .withColumn('include', F.when(col(user_col_name).isin(user_ids), True)
                                                      .otherwise(col('include'))))
 
-    def opt_out(self, user_ids):
+    def opt_out(self, user_ids: List[str]):
+        """
+        Update the user consent table based on list of user ids that have opted out
+        Args:
+            user_ids: list of user ids to flag as opted out, i.e. include = False
+        """
         user_col_name = self.user_consent.columns[0]
         self.user_consent = (self.user_consent
                              .withColumn('include', F.when(col(user_col_name).isin(user_ids), False)
