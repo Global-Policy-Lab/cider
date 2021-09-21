573902b21a6fee3035c98fe88364526b9d26c931
LucioMelito <lucio.melito@gmail.com>
2021-09-21 16:25:20 +0100

    type hints and data store integration for targeting module

--- datastore.py
+++ datastore.py
@@ -7,13 +7,14 @@ import inspect
 from helpers.io_utils import load_antennas, load_shapefile, load_cdr, load_mobilemoney, load_mobiledata, load_recharges
 from helpers.opt_utils import generate_user_consent_list
 from helpers.utils import get_spark_session, filter_dates_dataframe, make_dir, save_df
+import numpy as np
 import os
 import pandas as pd
 from pandas import DataFrame as PandasDataFrame, Series
 from pyspark.sql import DataFrame as SparkDataFrame
 import pyspark.sql.functions as F
 from pyspark.sql.functions import col, count, countDistinct, lit
-from typing import Callable, Dict, List, Optional, Union
+from typing import Callable, Dict, List, Mapping, Optional, Union
 import yaml
 
 
@@ -28,6 +29,7 @@ class DataType(Enum):
     POVERTY_SCORES = 7
     FEATURES = 8
     LABELS = 9
+    TARGETING = 10
 
 
 class InitializerInterface(ABC):
@@ -80,6 +82,8 @@ class DataStore(InitializerInterface):
         self.x: PandasDataFrame
         self.y: Series
         self.weights = None
+        # targeting
+        self.targeting: PandasDataFrame
 
         # Define mapping between data types and loading methods
         self.data_type_to_fn_map: Dict[DataType, Callable] = {DataType.CDR: self._load_cdr,
@@ -91,7 +95,8 @@ class DataStore(InitializerInterface):
                                                               DataType.HOME_GROUND_TRUTH: self._load_home_ground_truth,
                                                               DataType.POVERTY_SCORES: self._load_poverty_scores,
                                                               DataType.FEATURES: self._load_features,
-                                                              DataType.LABELS: self._load_labels}
+                                                              DataType.LABELS: self._load_labels,
+                                                              DataType.TARGETING: self._load_targeting}
 
     def _load_cdr(self, dataframe: Optional[Union[SparkDataFrame, PandasDataFrame]] = None) -> None:
         """
@@ -197,6 +202,30 @@ class DataStore(InitializerInterface):
             self.labels = self.labels.withColumn('weight', lit(1))
         self.labels = self.labels.select(['name', 'label', 'weight'])
 
+    def _load_targeting(self) -> None:
+        """
+        TO BE FILLED
+        """
+        self.targeting = pd.read_csv(self.data + self.file_names.targeting)
+        self.targeting['random'] = np.random.rand(len(self.data))
+
+        # Unweighted data
+        self.unweighted_targeting = self.targeting.copy()
+        self.unweighted_targeting['weight'] = 1
+
+        # Weighted data
+        self.weighted_targeting = self.targeting.copy()
+        if 'weight' not in self.weighted_targeting.columns:
+            self.data['weight'] = 1
+        else:
+            self.weighted_targeting['weight'] = (self.weighted_targeting['weight']/
+                                                 self.weighted_targeting['weight'].min())
+        self.weighted_data = pd.DataFrame(np.repeat(self.weighted_targeting.values,
+                                                    self.weighted_targeting['weight'],
+                                                    axis=0),
+                                          columns=self.weighted_targeting.columns)\
+            .astype(self.unweighted_targeting.dtypes)
+
     def merge(self) -> None:
         """
         Merge features and labels, split into x and y dataframes
@@ -219,7 +248,7 @@ class DataStore(InitializerInterface):
         # Make the smallest weight 1
         self.weights = self.merged['weight'] / self.merged['weight'].min()
 
-    def load_data(self, data_type_map: Dict[DataType, Optional[Union[SparkDataFrame, PandasDataFrame]]]) -> None:
+    def load_data(self, data_type_map: Mapping[DataType, Optional[Union[SparkDataFrame, PandasDataFrame]]]) -> None:
         """
         Load all datasets defined by data_type_map; raise an error if any of them failed to load
         Args:
--- targeting.py
+++ targeting.py
@@ -1,45 +1,39 @@
 # TODO: parallelize lasso and forward selection
-from box import Box
-import yaml
-from helpers.utils import *
-from helpers.plot_utils import *
-from helpers.io_utils import *
-from helpers.ml_utils import *
-from scipy.stats import spearmanr
-from sklearn.metrics import roc_auc_score, roc_curve, auc
+from datastore import DataStore, DataType
+from helpers.plot_utils import clean_plot, mtick
+from helpers.ml_utils import confusion_matrix, strictly_increasing
+from helpers.utils import make_dir
+import matplotlib.pyplot as plt  # type: ignore[import]
+import numpy as np
+import pandas as pd
+from pandas import DataFrame as PandasDataFrame
+from scipy.stats import spearmanr  # type: ignore[import]
+import seaborn as sns  # type: ignore[import]
+from sklearn.metrics import roc_auc_score, roc_curve, auc  # type: ignore[import]
+from typing import Dict, List, Optional, Tuple, Union
 
 
 class Targeting:
 
-    def __init__(self, cfg_dir, dataframe=None, clean_folders=False):
+    def __init__(self, datastore: DataStore, clean_folders: bool = False) -> None:
+        self.cfg = datastore.cfg
+        self.ds = datastore
+        self.outputs = datastore.outputs + 'featurizer/'
 
-        # Read config file
-        with open(cfg_dir, "r") as ymlfile:
-            cfg = Box(yaml.safe_load(ymlfile))
-        self.cfg = cfg
-        data_path = cfg.path.targeting.data + cfg.path.targeting.file_names.data
-        self.data = pd.read_csv(data_path)
-        self.data['random'] = np.random.rand(len(self.data))
-        outputs = cfg.path.targeting.outputs
-        self.outputs = outputs
-        file_names = cfg.path.targeting.file_names
+        # Prepare working directories, set color palette
+        make_dir(self.outputs, clean_folders)
         self.default_colors = sns.color_palette('Set2', 100)
 
-        # Unweighted data
-        self.unweighted_data = self.data.copy()
-        self.unweighted_data['weight'] = 1
+        # Load data into datastore
+        data_type_map = {DataType.TARGETING: None}
+        self.ds.load_data(data_type_map=data_type_map)
 
-        # Weighted data
-        self.weighted_data = self.data.copy()
-        if 'weight' not in self.weighted_data.columns:
-            self.data['weight'] = 1
-        else:
-            self.weighted_data['weight'] = self.weighted_data['weight']/self.weighted_data['weight'].min()
-        self.weighted_data = pd.DataFrame(np.repeat(self.weighted_data.values, self.weighted_data['weight'], axis=0), columns=self.weighted_data.columns)\
-            .astype(self.unweighted_data.dtypes)
-
-
-    def threshold_to_percentile(self, p, t, data, var, test_percentiles=True):
+    @staticmethod
+    def threshold_to_percentile(p: Optional[Union[float, int]],
+                                t: Optional[Union[float, int]],
+                                data: PandasDataFrame,
+                                var: str,
+                                test_percentiles: bool = True) -> Union[float, int]:
 
         # Make sure at least one of t or p is provided
         if p is None and t is None:
@@ -47,11 +41,11 @@ class Targeting:
 
         # Make sure not both t and p are provided
         if p is not None and t is not None:
-            raise ValueError('Cannot provide both percentage and threhsold targeting for var1')
+            raise ValueError('Cannot provide both percentage and threshold targeting for var1')
 
         # Make sure percentile is within range (0 to 100 exclusive)
         if test_percentiles and (p == 0 or p == 100):
-            raise ValueError('Pecentage targeting must be between 0 and 100 (exclusive).')
+            raise ValueError('Percentage targeting must be between 0 and 100 (exclusive).')
         
         # If t is provided, convert threshold to percentile
         if t is not None:
@@ -59,20 +53,20 @@ class Targeting:
         
         return p
 
-
-    def pearson(self, var1, var2, weighted=False):
-        data = self.weighted_data if weighted else self.unweighted_data
+    def pearson(self, var1: str, var2: str, weighted: bool = False) -> float:
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         return np.corrcoef(data[var1].astype('float'), data[var2].astype('float'))[0][1]
-    
 
-    def spearman (self, var1, var2, weighted=False):
-        data = self.weighted_data if weighted else self.unweighted_data
+    def spearman(self, var1: str, var2: str, weighted: bool = False) -> float:
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         return spearmanr(data[var1].astype('float'), data[var2].astype('float'))[0]
 
+    def binary_metrics(self, var1: str, var2: str,
+                       p1: Union[int, float], p2: Optional[Union[int, float]],
+                       t1: Optional[Union[int, float]] = None, t2: Optional[Union[int, float]] = None,
+                       weighted: bool = False) -> Dict[str, float]:
 
-    def binary_metrics(self, var1, var2, p1, p2, t1=None, t2=None, weighted=False):
-
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         results = {}
 
         # If thresholds are provided, convert to percentiles
@@ -108,11 +102,12 @@ class Targeting:
         results['tpr'] = tp/(tp + fn)
         results['fpr'] = fp/(fp + tn)
         return results
-    
 
-    def auc_threshold(self, var1, var2, p, t=None, weighted=False):
+    def auc_threshold(self, var1: str, var2: str,
+                      p: Optional[Union[int, float]], t: Optional[Union[int, float]] = None,
+                      weighted: bool = False) -> Dict[str, List[float]]:
 
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         results = {}
 
         # If threshold is provided, convert to percentile
@@ -135,12 +130,9 @@ class Targeting:
         results['tpr'] = roc[1]
 
         return results
-    
 
-    def auc_overall(self, var1, var2, weighted=False, n_grid=99):
-
-        data = self.weighted_data if weighted else self.unweighted_data
-
+    def auc_overall(self, var1: str, var2: str,
+                    weighted: bool = False, n_grid: int = 99) -> Dict[str, List[float]]:
         # Generate grid of thresholds between 0 and 100
         grid = np.linspace(1, 100, n_grid)[:-1]
 
@@ -149,8 +141,8 @@ class Targeting:
         tprs, fprs = [g['tpr'] for g in metrics_grid], [g['fpr'] for g in metrics_grid]
         
         # When threshold is 0, FPR + TPR are always 0; when threshold is 100, FPR + TPR are always 100 
-        fprs = [0] + fprs + [1]
-        tprs = [0] + tprs + [1]
+        fprs = [0.] + fprs + [1]
+        tprs = [0.] + tprs + [1]
         
         # Smooth any non-increasing parts of the vectors so we can calculate the AUC 
         while not strictly_increasing(fprs):
@@ -162,12 +154,14 @@ class Targeting:
             tprs = [tprs[i] for i in range(len(tprs)) if i not in to_remove]
         
         # Calculate the AUC score 
-        return {'fpr':fprs, 'tpr':tprs, 'auc':auc(fprs, tprs)} 
+        return {'fpr': fprs, 'tpr': tprs, 'auc': auc(fprs, tprs)}
 
+    def utility(self, var1: str, var2: str,
+                transfer_size: Union[float, int],
+                p: Optional[Union[int, float]], t: Optional[Union[int, float]] = None,
+                weighted: bool = False, rho: Union[int, float] = 3) -> float:
 
-    def utility(self, var1, var2, transfer_size, p, t=None, weighted=False, rho=3):
-
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         data = data[[var1, var2]].sort_values(var2, ascending=True)
 
         # If threshold is provided, convert to percentile
@@ -182,21 +176,24 @@ class Targeting:
         data['utility'] = ((data[var1] + data['benefits'])**(1-rho))/(1-rho)
 
         return data['utility'].sum()
-
     
-    def targeting_table(self, groundtruth, proxies, p1, p2, t1=None, weighted=False):
+    def targeting_table(self, groundtruth: str, proxies: List[str],
+                        p1: Optional[Union[int, float]], p2: Optional[Union[int, float]],
+                        t1: Optional[Union[int, float]] = None, weighted: bool = False) -> PandasDataFrame:
 
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
 
         # If thresholds are provided, convert to percentiles
         p1 = self.threshold_to_percentile(p1, t1, data, groundtruth)
 
-        # Results table: Spearman, Pearson, AUC (Threshold-Agnostic), Accuracy, Precision, Recall, AUC (Threshold-Specific)
+        # Results table: Spearman, Pearson, AUC (Threshold-Agnostic),
+        # Accuracy, Precision, Recall, AUC (Threshold-Specific)
         results = pd.DataFrame()
         results['Targeting Method'] = proxies 
         results['Pearson'] = [self.pearson(groundtruth, proxy, weighted=weighted) for proxy in proxies]
         results['Spearman'] = [self.spearman(groundtruth, proxy, weighted=weighted) for proxy in proxies]
-        results['AUC (Threshold-Agnostic)'] = [self.auc_overall(groundtruth, proxy, weighted=weighted)['auc'] for proxy in proxies]
+        results['AUC (Threshold-Agnostic)'] = [self.auc_overall(groundtruth, proxy, weighted=weighted)['auc']
+                                               for proxy in proxies]
         metrics = [self.binary_metrics(groundtruth, proxy, p1, p2, weighted=weighted) for proxy in proxies]
         results['Accuracy'] = [m['accuracy'] for m in metrics]
         results['Precision'] = [m['precision'] for m in metrics]
@@ -209,30 +206,33 @@ class Targeting:
         results.to_csv(self.outputs + '/targeting_table_' + pecentile_label + '.csv', index=False)
         return results
 
+    def roc_curves(self, groundtruth: str, proxies: List[str],
+                   p: Optional[Union[int, float]] = None, t: Optional[Union[int, float]] = None,
+                   weighted: bool = False, colors: Optional[Dict[int, str]] = None) -> None:
 
-    def roc_curves(self, groundtruth, proxies, p=None, t=None, weighted=False, colors=None):
-
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
         
         # If threshold is provided, convert to percentile
         if p is not None or t is not None:
             p = self.threshold_to_percentile(p, t, data, groundtruth)
 
-        # If threshold/percentile not provided, use threshold-agnostic ROC. If threshold/percentile is provided, use specific AUC.
+        # If threshold/percentile not provided, use threshold-agnostic ROC. If threshold/percentile is provided,
+        # use specific AUC.
         if p is None:
-            rocs = {proxy:self.auc_overall(groundtruth, proxy, weighted=weighted) for proxy in proxies}
+            rocs = {proxy: self.auc_overall(groundtruth, proxy, weighted=weighted) for proxy in proxies}
         else:
-            rocs = {proxy:self.auc_threshold(groundtruth, proxy, p, weighted=weighted) for proxy in proxies}
+            rocs = {proxy: self.auc_threshold(groundtruth, proxy, p, weighted=weighted) for proxy in proxies}
 
         # Set up color palette
         if colors is None:
             colors = self.default_colors
-        colors = {proxy: colors[p] for p, proxy in enumerate(proxies)}
+        colors_proxy = {proxy: colors[p] for p, proxy in enumerate(proxies)}
 
         # Plot ROC curves
         fix, ax = plt.subplots(1, figsize=(10, 8))
         for proxy in proxies:
-            ax.plot([100*x for x in rocs[proxy]['fpr']], [100*x for x in rocs[proxy]['tpr']], label=proxy, color=colors[proxy])
+            ax.plot([100*x for x in rocs[proxy]['fpr']], [100*x for x in rocs[proxy]['tpr']],
+                    label=proxy, color=colors_proxy[proxy])
         ax.plot([0, 100], [0, 100], color='grey', dashes=[1, 1], label='Random')
 
         # Clean up plot
@@ -254,33 +254,41 @@ class Targeting:
         plt.savefig(self.outputs + '/auc_curves' + pecentile_label + '.png', dpi=400)
         plt.show()
 
+    def precision_recall_curves(self, groundtruth: str, proxies: List[str],
+                                p: Optional[Union[int, float]] = None, t: Optional[Union[int, float]] = None,
+                                weighted: bool = False, n_grid: int = 99,
+                                colors: Optional[Dict[int, str]] = None) -> None:
 
-    def precision_recall_curves(self, groundtruth, proxies, p=None, t=None, weighted=False, n_grid=99, colors=None):
-
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
 
         # If threshold is provided, convert to percentile
         if p is not None or t is not None:
             p = self.threshold_to_percentile(p, t, data, groundtruth)
 
-        # If threshold/percentile is not provided, use balanced grid. Otherwise, use threshold provided for threshold-specific measures.
+        # If threshold/percentile is not provided, use balanced grid. Otherwise, use threshold provided for
+        # threshold-specific measures.
         grid = np.linspace(1, 100, n_grid)[:-1]
         if p is None:
-            metrics_grid = {proxy:[self.binary_metrics(groundtruth, proxy, p2, p2, weighted=weighted) for p2 in grid] for proxy in proxies}
+            metrics_grid = {proxy: [self.binary_metrics(groundtruth, proxy, p2, p2, weighted=weighted) for p2 in grid]
+                            for proxy in proxies}
         else:
-            metrics_grid = {proxy:[self.binary_metrics(groundtruth, proxy, p, p2, weighted=weighted) for p2 in grid] for proxy in proxies}
+            metrics_grid = {proxy: [self.binary_metrics(groundtruth, proxy, p, p2, weighted=weighted) for p2 in grid]
+                            for proxy in proxies}
 
         # Set up color palette
         if colors is None:
             colors = self.default_colors
-        colors = {proxy: colors[p] for p, proxy in enumerate(proxies)}
+        colors_proxy = {proxy: colors[p] for p, proxy in enumerate(proxies)}
 
-        # Plot precision and recall curves (only precision if using balanced grid, since precision and recall are the same)
+        # Plot precision and recall curves (only precision if using balanced grid, since precision and recall are the
+        # same)
         fig, ax = plt.subplots(1, figsize=(10, 8))
         for proxy in proxies:
-            ax.plot(grid, [100*metrics_grid[proxy][i]['precision'] for i in range(len(grid))], label=proxy, color=colors[proxy])
+            ax.plot(grid, [100*metrics_grid[proxy][i]['precision'] for i in range(len(grid))],
+                    label=proxy, color=colors_proxy[proxy])
             if p is not None:
-                ax.plot(grid, [100*metrics_grid[proxy][i]['recall'] for i in range(len(grid))], dashes=[2, 2], color=colors[proxy])
+                ax.plot(grid, [100*metrics_grid[proxy][i]['recall'] for i in range(len(grid))],
+                        dashes=[2, 2], color=colors_proxy[proxy])
         
         # Clean up plot
         plt.legend(loc='best')
@@ -304,15 +312,16 @@ class Targeting:
         plt.savefig(self.outputs + '/precision_recall_curves' + pecentile_label + '.png', dpi=400)
         plt.show()
 
+    def utility_grid(self, groundtruth: str, proxies: List[str],
+                     ubi_transfer_size: Union[int, float], weighted: bool = False, n_grid: int = 99
+                     ) -> Tuple[np.ndarray, List[float], Dict[str, List[float]]]:
 
-    def utility_grid(self, groundtruth, proxies, ubi_transfer_size, weighted=False, n_grid=99):
-
-        data = self.weighted_data if weighted else self.unweighted_data
+        data = self.ds.weighted_targeting if weighted else self.ds.unweighted_targeting
 
         # Set up grid and results 
         budget = ubi_transfer_size*len(data)
         grid = np.linspace(1, 100, n_grid)
-        utilities = {proxy:[] for proxy in proxies}
+        utilities: Dict[str, List[float]] = {proxy: [] for proxy in proxies}
         transfer_sizes = []
 
         # Calculate utility and transfer size for each proxy and each value in grid
@@ -321,30 +330,33 @@ class Targeting:
             transfer_size = budget/num_targeted
             transfer_sizes.append(transfer_size)
             for proxy in proxies:
-                utilities[proxy] = utilities[proxy] + [self.utility(groundtruth, proxy, transfer_size, p, weighted=weighted)]
+                utilities[proxy] = utilities[proxy] + \
+                                   [self.utility(groundtruth, proxy, transfer_size, p, weighted=weighted)]
 
         return grid, transfer_sizes, utilities
 
-
-    def utility_curves(self, groundtruth, proxies, ubi_transfer_size, weighted=False, n_grid=99, colors=None):
+    def utility_curves(self, groundtruth: str, proxies: List[str],
+                       ubi_transfer_size: Union[int, float], weighted: bool = False,
+                       n_grid: int = 99, colors: Optional[Dict[int, str]] = None) -> None:
 
         # Get utility grid
-        grid, _, utilities = self.utility_grid(groundtruth, proxies, ubi_transfer_size, weighted=weighted, n_grid=n_grid)
+        grid, _, utilities = self.utility_grid(groundtruth, proxies, ubi_transfer_size, weighted=weighted,
+                                               n_grid=n_grid)
 
         # Set up color palette
         if colors is None:
             colors = self.default_colors
-        colors = {proxy: colors[p] for p, proxy in enumerate(proxies)}
+        colors_proxy = {proxy: colors[p] for p, proxy in enumerate(proxies)}
         
         # Plot utility curves for each proxy
         fix, ax = plt.subplots(1, figsize=(10, 8))
         for proxy in proxies:
-            ax.plot(grid, utilities[proxy], color=colors[proxy], label=proxy)
+            ax.plot(grid, utilities[proxy], color=colors_proxy[proxy], label=proxy)
 
         # Plot circle for maximum utility for each proxy
         for proxy in proxies:
             max_utility_idx = np.argmax(utilities[proxy])
-            ax.scatter([grid[max_utility_idx]], [utilities[proxy][max_utility_idx]], color=colors[proxy], s=80)
+            ax.scatter([grid[max_utility_idx]], [utilities[proxy][max_utility_idx]], color=colors_proxy[proxy], s=80)
         
         # Add horizontal line for UBI
         ax.axhline(utilities[proxies[0]][-1], color='grey', dashes=[1, 1], label='UBI')
@@ -362,11 +374,13 @@ class Targeting:
         plt.savefig(self.outputs + '/utility_curves.png', dpi=400)
         plt.show()
 
-
-    def utility_table(self, groundtruth, proxies, ubi_transfer_size, weighted=False, n_grid=99):
+    def utility_table(self, groundtruth: str, proxies: List[str],
+                      ubi_transfer_size: Union[int, float], weighted: bool = False,
+                      n_grid: int = 99) -> PandasDataFrame:
 
         # Get utility grid and transfer size grid
-        population_shares, transfer_sizes, utilities = self.utility_grid(groundtruth, proxies, ubi_transfer_size, weighted=weighted, n_grid=n_grid)
+        population_shares, transfer_sizes, utilities = self.utility_grid(groundtruth, proxies, ubi_transfer_size,
+                                                                         weighted=weighted, n_grid=n_grid)
         
         # Create table with best population share, utility, and transfer size for each proxy
         table_population_shares, table_utilities, table_transfer_sizes = [], [], []
